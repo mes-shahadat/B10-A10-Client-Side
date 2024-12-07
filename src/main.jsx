549da66b5fa21bcdf899d9effa0d27b1d6022c27
mohammad yasin <binodonprojukti@gmail.com>
2024-12-07 22:54:35 +0600

    imported auth functions

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/UserRouter'
 import { AuthProvider } from './utils/AuthProvider'
-
-import "keen-slider/keen-slider.min.css"
-import './index.css'
 import { LocalStorageProvider } from './utils/LocalStorageProvider'
 
+import "keen-slider/keen-slider.min.css"
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/utils/AuthProvider.jsx
+++ src/utils/AuthProvider.jsx
@@ -1,13 +1,140 @@
-import { createContext } from "react"
+import { createContext, useEffect, useState } from "react"
+import { createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword, onAuthStateChanged, signOut, sendPasswordResetEmail, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "./firebase_init";
+import { toast } from 'react-toastify';
 
 const AuthContext = createContext()
 
 const AuthProvider = ({ children }) => {
 
-    const authData = {
-        name: "mohammad yasin"
+    
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const loginGoogleUser = () => {
+
+        const provider = new GoogleAuthProvider();
+
+        signInWithPopup(auth, provider)
+            .then(() => {
+                toast.success("Log In successfull", {
+                    position: "bottom-right"
+                })
+            }).catch((error) => {
+                toast.error(error.message, {
+                    position: "bottom-right"
+                })
+            });
     }
 
+    const createUser = (email, password, callback) => {
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                callback()
+                toast.success("Registration successfull", {
+                    position: "bottom-right"
+                })
+            })
+            .catch((error) => {
+                toast.error(error.message, {
+                    position: "bottom-right"
+                })
+            });
+    }
+
+    const updateUser = (obj) => {
+
+        updateProfile(auth.currentUser, obj)
+            .then(() => {
+                toast.success("Profile update successfull", {
+                    position: "bottom-right"
+                })
+                setUser({ ...user, ...obj })
+            }).catch((error) => {
+                toast.error(error.message, {
+                    position: "bottom-right"
+                })
+            });
+    }
+
+    const loginUser = (email, password) => {
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+
+                toast.success("Login successfull", {
+                    position: "bottom-right"
+                })
+
+            })
+            .catch((error) => {
+                toast.error(error.message, {
+                    position: "bottom-right"
+                })
+            });
+    }
+
+    const logOut = () => {
+
+        signOut(auth).then(() => {
+
+            toast.success("Log Out successfull", {
+                position: "bottom-right"
+            })
+        }).catch((error) => {
+
+            toast.error(error.message, {
+                position: "bottom-right"
+            })
+        });
+    }
+
+    const resetUserPassword = (email) => {
+
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                toast.success("Email Sent", {
+                    position: "bottom-right"
+                })
+            })
+            .catch((error) => {
+                toast.error(error.message, {
+                    position: "bottom-right"
+                })
+            });
+    }
+
+    useEffect(
+        () => {
+            let unsubscribe = onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    setUser(user);
+                } else {
+                    setUser(null);
+                }
+                
+                setLoading(false);
+            })
+
+            return () => { unsubscribe() }
+
+        }, []
+    )
+
+    const authData = {
+
+        user,
+        loading,
+        setUser,
+        createUser,
+        loginUser,
+        logOut,
+        updateUser,
+        resetUserPassword,
+        loginGoogleUser, 
+        setLoading
+    }
     return (
         <AuthContext.Provider value={authData}>
             {children}
