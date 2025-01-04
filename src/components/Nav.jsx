05ea749b2c79ce92277914b551eac7b0e232eca7
mohammad yasin <binodonprojukti@gmail.com>
2025-01-04 16:14:51 +0600

    navbar fixed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,7 +14,7 @@ export const Nav = () => {
 
 
   useEffect(() => {
-    
+
     const toggle = document.querySelector('.navbar .theme-controller');
     const update = () => {
       setRadioChecked(prev => !prev)
@@ -60,27 +60,36 @@ export const Nav = () => {
         <div className="navbar md:w-11/12 mx-auto">
 
           <div className="navbar-start">
-            <div className="dropdown">
-              <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <div className="dropdown ml-2">
+              <label className="btn btn-circle swap swap-rotate lg:hidden">
+                {/* this hidden checkbox controls the state */}
+                <input type="checkbox" />
+
+                {/* hamburger icon */}
                 <svg
+                  className="swap-off fill-current"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16" />
+                  width="32"
+                  height="32"
+                  viewBox="0 0 512 512">
+                  <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
                 </svg>
-              </div>
+
+                {/* close icon */}
+                <svg
+                  className="swap-on fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 512 512">
+                  <polygon
+                    points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+                </svg>
+              </label>
               <ul
                 tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                {
-                  navlinks
-                }
+                className="menu menu-sm dropdown-custom-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow  border border-base-content/10 lg:hidden">
+                {navlinks}
               </ul>
             </div>
             <Link className="btn btn-ghost text-xl" to="/">
@@ -132,7 +141,7 @@ export const Nav = () => {
                   /></Link>
                 <NavLink className="btn " onClick={
                   () => {
-                    setTimeout(()=>navigate("/"), 100)
+                    setTimeout(() => navigate("/"), 100)
                     logOut()
                   }
                 }>Log Out</NavLink>
